f1c0c7f5299a31a2cc3a5c51405b442d4331593b
Oliver Masters <contact@olivermasters.co.uk>
2023-06-15 14:46:01 +0100

    add db

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 import os
+from pathlib import Path
 
 
-class Config(object):
+basedir = Path(__file__).resolve().parent
+
+
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    db_path = basedir / 'app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + str(db_path)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
